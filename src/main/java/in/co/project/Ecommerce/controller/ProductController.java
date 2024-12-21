3f38db429230d772e49f6d6a35f8ab11f3667e68
MayankRaje <mayankraje7000@gmail.com>
2024-12-21 13:25:44 +0530

    Implemented getAll EP and delete EP with void return type

--- src/main/java/in/co/project/Ecommerce/controller/ProductController.java
+++ src/main/java/in/co/project/Ecommerce/controller/ProductController.java
@@ -5,6 +5,10 @@ import in.co.project.Ecommerce.models.Product;
 import org.springframework.web.bind.annotation.*;
 import in.co.project.Ecommerce.service.FakestoreProductService;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class ProductController {
 
@@ -29,12 +33,21 @@ public class ProductController {
         Product product=productService.getSingleProduct(id);
         return product;
     }
+    //GET list of products
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){ // HW
+        List<Product> product=productService.getAllProducts();
+        System.out.println(product);
+        return product;
+    }
     //update product
     public void updateProduct( Product product) {
 
     }
     //delete product
-    public void deleteProduct(Long id) {
+    @DeleteMapping("/products/{id}")
+    public void deleteSingleProduct(@PathVariable("id") Long id) { //HW(del EP with void RT)
+         productService.deleteSingleProduct(id);
 
     }
 
--- src/main/java/in/co/project/Ecommerce/service/ProductService.java
+++ src/main/java/in/co/project/Ecommerce/service/ProductService.java
@@ -8,7 +8,10 @@ public interface ProductService {
     Product getSingleProduct(Long id);
     List<Product> getAllProducts();
     //Product createProduct(Product product);
+    void deleteSingleProduct(Long id);
     Product createProduct(Long id, String title, String description, Double price, String category, String image);
+   // Product updateProduct(Long id, String title, Double price, String description, String image, String category);
+
 
 
 }
