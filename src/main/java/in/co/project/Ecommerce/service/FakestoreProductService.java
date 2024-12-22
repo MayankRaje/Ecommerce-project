6f4ae0f6700f8dc4501e87a61d49c0db3d88843f
MayankRaje <mayankraje7000@gmail.com>
2024-12-22 15:18:17 +0530

    Implemented all end points in various  ways

--- src/main/java/in/co/project/Ecommerce/service/FakestoreProductService.java
+++ src/main/java/in/co/project/Ecommerce/service/FakestoreProductService.java
@@ -3,7 +3,7 @@ package in.co.project.Ecommerce.service;
 import in.co.project.Ecommerce.dto.FakestoreProductDto;
 import in.co.project.Ecommerce.models.Category;
 import in.co.project.Ecommerce.models.Product;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
@@ -23,7 +23,7 @@ public class FakestoreProductService implements ProductService{
     @Override
     public Product getSingleProduct(Long id) {
         FakestoreProductDto fakestoreProductDto=
-                restTemplate.getForObject("https://fakestoreapi.com/products/1"+id, FakestoreProductDto.class);
+                restTemplate.getForObject("https://fakestoreapi.com/products/"+id, FakestoreProductDto.class);
         //System.out.println("returning single product method");
         // convert FakestoreProductDtO to my Product
 
@@ -63,22 +63,41 @@ public class FakestoreProductService implements ProductService{
         return response.getProduct();
     }
 
+    @Override
+    public Product updateProduct(Long id, String title, Double price, String description, String image, String category) {
+        Product product=getSingleProduct(id);
+        FakestoreProductDto fakestoreProductDto=new FakestoreProductDto();
+        fakestoreProductDto.setId(product.getId());
+        fakestoreProductDto.setTitle(product.getTitle());
+        fakestoreProductDto.setPrice(product.getPrice());
+        fakestoreProductDto.setDescription(product.getDescription());
+        fakestoreProductDto.setCategory(product.getCategory().getTitle());
+        fakestoreProductDto.setImage(image);
+
+        restTemplate.put("https://fakestoreapi.com/products/"+id,fakestoreProductDto
+                ,FakestoreProductDto.class);
+//        HttpHeaders headers=new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        HttpEntity<Product>entity=new HttpEntity<>(product,headers);
+//        restTemplate.exchange("https://fakestoreapi.com/products/"+id, HttpMethod.PUT,entity
+//                ,Product.class);
+//
+//
+        return fakestoreProductDto.getProduct();
+    }
+
 //    @Override
-//    public Product updateProduct(Long id, String title, Double price, String description, String image, String category) {
+//    public Product updateProduct(Long id,Product product) {
+//        Product product=getSingleProduct(id);
 //        FakestoreProductDto fakestoreProductDto=new FakestoreProductDto();
-//        fakestoreProductDto.setId(id);
-//        fakestoreProductDto.setTitle(title);
-//        fakestoreProductDto.setPrice(price);
-//        fakestoreProductDto.setDescription(description);
-//        fakestoreProductDto.setCategory(category);
+//        fakestoreProductDto.setId(product.getId());
+//        fakestoreProductDto.setTitle(product.getTitle());
+//        fakestoreProductDto.setPrice(product.getPrice());
+//        fakestoreProductDto.setDescription(product.getDescription());
+//        fakestoreProductDto.setCategory(product.getCategory().getTitle());
 //        fakestoreProductDto.setImage(image);
 //
-//        FakestoreProductDto response=restTemplate.exc("https://fakestoreapi.com/products",
-//                fakestoreProductDto,FakestoreProductDto.class);
-//        return response.getProduct();
-//
-//
-//        return null;
+//        return fakestoreProductDto.getProduct();
 //    }
 
 //    @Override
@@ -86,11 +105,31 @@ public class FakestoreProductService implements ProductService{
 //        return null;
 //    }
 
-      public void deleteSingleProduct(Long id) {
+//      @Override
+//      public void deleteSingleProduct(Long id) { //1 VOID RETURN TYPE
+//        //M==>1 ==> Itetate ans skip an id
+//        //M==>2
+//        restTemplate.delete("https://fakestoreapi.com/products/"+ id,id);
+//
+//      }
+
+    @Override
+    public Product deleteSingleProduct(Long id) { //RETURN PRODUCT IN POSTMAN
         //M==>1 ==> Itetate ans skip an id
         //M==>2
+        Product product = getSingleProduct(id);
         restTemplate.delete("https://fakestoreapi.com/products/"+ id,id);
+        return product;
+    }
 
-      }
+    //@Override
+//    public Product deleteSingleProduct(Product product1) { //implement via response body from POSTMAN
+//        //M==>1 ==> Itetate ans skip an id
+//        //M==>2
+//        Long id=product1.getId();
+//        Product product=getSingleProduct(product1.getId());
+//        restTemplate.delete("https://fakestoreapi.com/products/"+ id,id);
+//        return product;
+//    }
 
 }
--- src/main/java/in/co/project/Ecommerce/service/ProductService.java
+++ src/main/java/in/co/project/Ecommerce/service/ProductService.java
@@ -8,9 +8,12 @@ public interface ProductService {
     Product getSingleProduct(Long id);
     List<Product> getAllProducts();
     //Product createProduct(Product product);
-    void deleteSingleProduct(Long id);
+    //void deleteSingleProduct(Long id);//1
+    Product deleteSingleProduct(Long id);//2
+    //Product deleteSingleProduct(Product product);//3
+
     Product createProduct(Long id, String title, String description, Double price, String category, String image);
-   // Product updateProduct(Long id, String title, Double price, String description, String image, String category);
+    Product updateProduct(Long id, String title, Double price, String description, String image, String category);
 
 
 
